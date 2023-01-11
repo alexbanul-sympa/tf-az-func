c1e29c691617e885f2d600dbf488370eca37a591
Alex Banul <alex.banul@sympa.com>
2023-01-11 13:09:14 +0200

    init commit

---
+++ main.tf
@@ -0,0 +1,48 @@
+﻿# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  location = "westeurope"
+  name     = "alba-tf-az-func-rg"
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = "sympaalbatfazfuncsa"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_service_plan" "sp" {
+  name                = "sympa-alba-az-func-sp"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "Y1"
+}
+
+resource "azurerm_linux_function_app" "example" {
+  name                = "sympa-alba-test-func-app"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  storage_account_name       = azurerm_storage_account.sa.name
+  storage_account_access_key = azurerm_storage_account.sa.primary_connection_string
+  service_plan_id            = azurerm_service_plan.sp.id
+
+  site_config {}
+}
